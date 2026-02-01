d44841e977621a7727a2c5b92f8505ff3ad1584a
Kevin Campos Venegas <kenderman.8@gmail.com>
2026-02-01 11:23:39 -0300

    a

--- JS/src/events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'ready',
-  once: true,
-  execute(interaction, client) {
-    console.log(`Ready! Logged in as ${client.user.tag}`);
-  },
-};
--- JS/src/functions/deletecommands.js
+++
@@ -1,8 +0,0 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-const {Routes, REST} = require('discord.js');
-
-const rest = new REST({version: '10'}).setToken(process.env.DISCORD_TOKEN);
-
-rest.put(Routes.applicationCommands(process.env.APPLICATION_ID), {body: []});
